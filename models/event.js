01a0f828434e13d141d80d4acc4fedf9380a39c1
Prasham <ppipaliy@uncc.edu>
2024-03-07 15:28:39 -0500

    merge conflict

--- models/event.js
+++ models/event.js
@@ -23,6 +23,17 @@ const events = [
         endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
         image: '',
         createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    },
+    {
+        id: '3',
+        topic: 'Free stuff',
+        title: 'Free stuff 2.0',
+        description: 'Learn programming',
+        location: 'Woodward Hall',
+        startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
+        endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
+        image: '',
+        createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
     }
     // {
     //     id: '2',
