9b7f1e0767512a0e67594680a6836b2f21b8e552
Prasham <ppipaliy@uncc.edu>
2024-03-11 18:35:33 -0400

    some changes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,89 +1,165 @@
-const model = require('../models/event');
+// const model = require('../models/event');
 
-//GET /events: send all the events
-exports.index = (req, res) => {
-    // res.send('send all the stories');
-    let events = model.find();
-    res.render('./event/index', { events });
-};
+// //GET /events: send all the events
+// exports.index = (req, res) => {
+//     // res.send('send all the stories');
+//     let events = model.find();
+//     res.render('./event/index', { events });
+// };
 
-// GET /events/new
-exports.new = (req, res) => {
-    // res.send('send the new form');
-    res.render('./event/new');
-};
+// // GET /events/new
+// exports.new = (req, res) => {
+//     // res.send('send the new form');
+//     res.render('./event/new');
+// };
 
-//POST /events
-exports.create = (req, res) => {
-    //res.send('created a new story');
-    console.log(req.body);
-    let event = req.body;
-    model.save(event);
-    res.redirect('/events');
-};
+// //POST /events
+// exports.create = (req, res) => {
+//     //res.send('created a new story');
+//     console.log(req.body);
+//     let event = req.body;
+//     model.save(event);
+//     res.redirect('/events');
+// };
 
-//GET /events/:id
-exports.show = (req, res, next) => {
-    let id = req.params.id;
-    let event = model.findById(id);
-    // res.send('send event with id ' + req.params.id);
-    if (event) {
-        res.render('./event/show', { event });
-    }
-    else {
-        // res.status(404).send('Cannot find event with id: ' + id);
-        let err = Error("Cannot find event with id " + id);
-        err.status = 404;
-        next(err);
-    }
+// //GET /events/:id
+// exports.show = (req, res, next) => {
+//     let id = req.params.id;
+//     let event = model.findById(id);
+//     // res.send('send event with id ' + req.params.id);
+//     if (event) {
+//         res.render('./event/show', { event });
+//     }
+//     else {
+//         // res.status(404).send('Cannot find event with id: ' + id);
+//         let err = Error("Cannot find event with id " + id);
+//         err.status = 404;
+//         next(err);
+//     }
 
-};
+// };
 
-//GET /events/:id/edit:
-exports.edit = (req, res, next) => {
-    //res.send('send the edit form');
-    let id = req.params.id;
-    let event = model.findById(id);
-    if (event) {
-        res.render('./event/edit', { event });
-    }
-    else {
-        // res.status(404).send('Cannot find event with id: ' + id);
-        let err = Error("Cannot find event with id " + id);
-        err.status = 404;
-        next(err);
-    }
-};
+// //GET /events/:id/edit:
+// exports.edit = (req, res, next) => {
+//     //res.send('send the edit form');
+//     let id = req.params.id;
+//     let event = model.findById(id);
+//     if (event) {
+//         res.render('./event/edit', { event });
+//     }
+//     else {
+//         // res.status(404).send('Cannot find event with id: ' + id);
+//         let err = Error("Cannot find event with id " + id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
 
-//PUT /events/:id
-exports.update = (req, res, next) => {
-    //res.status(200).send(`update event with id: ${req.params.id}`);
-    //res.send('update event with id: ', req.params.id);
-    let event = req.body;
-    //console.log(event);
-    let id = req.params.id;
-    if (model.updateById(id, event)) {
-        res.redirect('/events/' + id);
-    }
-    else {
-        // res.status(404).send('Cannot find event with id: ' + id);
-        let err = Error("Cannot find event with id " + id);
-        err.status = 404;
-        next(err);
-    }
-};
+// //PUT /events/:id
+// exports.update = (req, res, next) => {
+//     //res.status(200).send(`update event with id: ${req.params.id}`);
+//     //res.send('update event with id: ', req.params.id);
+//     let event = req.body;
+//     //console.log(event);
+//     let id = req.params.id;
+//     if (model.updateById(id, event)) {
+//         res.redirect('/events/' + id);
+//     }
+//     else {
+//         // res.status(404).send('Cannot find event with id: ' + id);
+//         let err = Error("Cannot find event with id " + id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
 
-//DELETE /events/:id
-exports.delete = (req, res, next) => {
-    //res.send('delete event with id: ', req.params.id);
-    let id = req.params.id;
-    if (model.deleteById(id)) {
-        res.redirect('/events');
+// //DELETE /events/:id
+// exports.delete = (req, res, next) => {
+//     //res.send('delete event with id: ', req.params.id);
+//     let id = req.params.id;
+//     if (model.deleteById(id)) {
+//         res.redirect('/events');
+//     }
+//     else {
+//         // res.status(404).send('Cannot find event with id: ' + id);
+//         let err = Error("Cannot find event with id " + id);
+//         err.status = 404;
+//         next(err);
+//     }
+// };
+
+
+const multer = require("multer");
+
+// configure multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now());
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10 * 1024 * 1024 }, // Adjust the file size limit as needed
+});
+
+// ...
+
+// POST /events
+router.post("/", upload.single("image"), (req, res, next) => {
+    // Handle image upload
+    const image = req.file ? "/uploads/" + req.file.filename : "";
+
+    // Retrieve other form data
+    const { topic, title, description, location, startTime, endTime } = req.body;
+
+    // Create a new event
+    const newEvent = {
+        topic,
+        title,
+        description,
+        location,
+        startTime,
+        endTime,
+        image,
+    };
+
+    // Save the event
+    events.save(newEvent);
+
+    // Redirect to the event show page or any other desired page
+    res.redirect(`/events/${newEvent.id}`);
+});
+
+// PUT /events/:id
+router.put("/:id", upload.single("image"), (req, res, next) => {
+    // Handle image upload
+    const image = req.file ? "/uploads/" + req.file.filename : "";
+
+    // Retrieve other form data
+    const { topic, title, description, location, startTime, endTime } = req.body;
+
+    // Update the event by ID
+    const updatedEvent = {
+        topic,
+        title,
+        description,
+        location,
+        startTime,
+        endTime,
+        image,
+    };
+
+    const success = events.updateById(req.params.id, updatedEvent);
+
+    if (success) {
+        // Redirect to the updated event show page or any other desired page
+        res.redirect(`/events/${req.params.id}`);
+    } else {
+        // Handle error, e.g., event not found
+        res.status(404).send("Event not found");
     }
-    else {
-        // res.status(404).send('Cannot find event with id: ' + id);
-        let err = Error("Cannot find event with id " + id);
-        err.status = 404;
-        next(err);
-    }
-};
+});
--- models/event.js
+++ models/event.js
@@ -1,41 +1,138 @@
+// const { DateTime } = require("luxon");
+// const { v4: uuidv4 } = require("uuid");
+// const path = require("path");
+
+// const events = [
+//   {
+//     id: '1',
+//     topic: 'Education',
+//     title: 'Programming NetBeans',
+//     description: 'Learn programming',
+//     location: 'Woodward Hall',
+//     startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
+//     endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
+//     image: '',
+//     createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+//   },
+//   {
+//     id: '2',
+//     topic: 'Free stuff',
+//     title: 'Free stuff',
+//     description: 'Learn programming',
+//     location: 'Woodward Hall',
+//     startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
+//     endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
+//     image: '',
+//     createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+//   },
+//   {
+//     id: '3',
+//     topic: 'Free stuff',
+//     title: 'Free stuff 2.0',
+//     description: 'Learn programming',
+//     location: 'Woodward Hall',
+//     startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
+//     endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
+//     image: '',
+//     createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+//   }
+// ];
+
+// exports.find = function () {
+//   return events;
+// };
+
+// exports.findById = function (id) {
+//   return events.find((event) => event.id === id);
+// };
+
+// exports.save = function (event) {
+//   event.id = uuidv4();
+//   event.createdAt = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT);
+
+//   events.push(event);
+// };
+
+// exports.updateById = function (id, newEvent) {
+//   let event = events.find(event => event.id === id);
+//   if (event) {
+
+//     event.topic = newEvent.topic;
+//     event.title = newEvent.title;
+//     event.description = newEvent.description;
+//     event.location = newEvent.location;
+//     event.startTime = newEvent.startTime;
+//     event.endTime = newEvent.endTime;
+//     event.image = newEvent.image;
+
+//     // Handle image update
+//     if (newEvent.image) {
+//       event.image = "/uploads/" + newEvent.image; // Adjust the path as needed
+
+//     } else {
+//       // If newEvent.image is an empty string, set event.image to an empty string
+//       event.image = "";
+
+
+//       return false;
+//     }
+//     return true;
+//   }
+
+//   else {
+//     return false;
+//   }
+// }
+
+// exports.deleteById = function (id) {
+//   let index = events.findIndex((event) => event.id === id);
+//   if (index !== -1) {
+//     events.splice(index, 1);
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+
 const { DateTime } = require("luxon");
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
 
 const events = [
   {
-    id: '1',
-    topic: 'Education',
-    title: 'Programming NetBeans',
-    description: 'Learn programming',
-    location: 'Woodward Hall',
+    id: "1",
+    topic: "Education",
+    title: "Programming NetBeans",
+    description: "Learn programming",
+    location: "Woodward Hall",
     startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
     endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
-    image: '',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    image: "",
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
   },
   {
-    id: '2',
-    topic: 'Free stuff',
-    title: 'Free stuff',
-    description: 'Learn programming',
-    location: 'Woodward Hall',
+    id: "2",
+    topic: "Free stuff",
+    title: "Free stuff",
+    description: "Learn programming",
+    location: "Woodward Hall",
     startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
     endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
-    image: '',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    image: "",
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
   },
   {
-    id: '3',
-    topic: 'Free stuff',
-    title: 'Free stuff 2.0',
-    description: 'Learn programming',
-    location: 'Woodward Hall',
+    id: "3",
+    topic: "Free stuff",
+    title: "Free stuff 2.0",
+    description: "Learn programming",
+    location: "Woodward Hall",
     startTime: DateTime.toLocaleString(2024, 2, 23, 15, 45),
     endTime: DateTime.toLocaleString(2024, 2, 23, 16, 45),
-    image: '',
-    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
-  }
+    image: "",
+    createdAt: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+  },
 ];
 
 exports.find = function () {
@@ -54,9 +151,8 @@ exports.save = function (event) {
 };
 
 exports.updateById = function (id, newEvent) {
-  let event = events.find(event => event.id === id);
+  let event = events.find((event) => event.id === id);
   if (event) {
-
     event.topic = newEvent.topic;
     event.title = newEvent.title;
     event.description = newEvent.description;
@@ -68,21 +164,17 @@ exports.updateById = function (id, newEvent) {
     // Handle image update
     if (newEvent.image) {
       event.image = "/uploads/" + newEvent.image; // Adjust the path as needed
-
     } else {
       // If newEvent.image is an empty string, set event.image to an empty string
       event.image = "";
 
-
       return false;
     }
     return true;
-  }
-
-  else {
+  } else {
     return false;
   }
-}
+};
 
 exports.deleteById = function (id) {
   let index = events.findIndex((event) => event.id === id);
